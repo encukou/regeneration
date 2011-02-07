16ae3ff305f89993db36e086cb241dae86ca1e46
Petr Viktorin <encukou@gmail.com>
2011-02-07 23:10:44 +0200

    A Battler class

---
+++ regeneration/battle/battler.py
@@ -0,0 +1,80 @@
+#! /usr/bin/python
+# Encoding: UTF-8
+
+from fractions import Fraction
+from collections import namedtuple
+from functools import partial
+
+from regeneration.battle.stats import Stats, BattleStat
+from regeneration.battle.move import Move
+
+__copyright__ = 'Copyright 2009-2011, Petr Viktorin'
+__license__ = 'MIT'
+__email__ = 'encukou@gmail.com'
+
+class Battler(object):
+    """A currently-sent-out monster.
+
+    This class includes everything that can be set on a Monster in battle.
+    It synchronizes whatever it should (HP, status, item changes) with the
+    Monster, but keeps volatile things (stat/ability/move changes) to itself,
+    to be forgotten when a battle ends.
+    """
+    def __init__(self, monster, spot):
+        self.monster = monster
+        self.species = monster.species
+        self.spot = spot
+
+        self.ability = monster.ability
+
+        self.stats = monster.stats
+        self.statLevels = Stats(0, statEnum=BattleStat)
+
+        self.moves = list(monster.moves)
+
+        self.level = monster.level
+        self.types = monster.types
+
+    @property
+    def fainted(self):
+        return self.hp <= 0
+
+    @property
+    def name(self):
+        return self.monster.name
+
+    @property
+    def status(self):
+        return self.monster.status
+
+    @status.setter
+    def status(self, value):
+        self.monster.status = value
+
+    @property
+    def hp(self):
+        return self.monster.hp
+
+    @hp.setter
+    def hp(self, value):
+        self.monster.hp = value
+
+    @property
+    def tameness(self):
+        return self.monster.tameness
+
+    @property
+    def gender(self):
+        return self.monster.gender
+
+    @property
+    def item(self):
+        return self.monster.item
+
+    @item.setter
+    def item(self, newItem):
+        self.monster.item = newItem
+
+    def setMove(self, i, kind):
+        self.moves = list(self.moves)
+        self.moves[i] = Move(kind)
---
+++ regeneration/battle/test/test_battler.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python
+# Encoding: UTF-8
+
+from nose.tools import assert_equal, assert_almost_equal
+import yaml
+import difflib
+
+from regeneration.battle.example import connect, tables, loader
+
+from regeneration.battle import battler, monster
+
+__copyright__ = 'Copyright 2011, Petr Viktorin'
+__license__ = 'MIT'
+__email__ = 'encukou@gmail.com'
+
+class FakeRand(object):
+    def randint(self, a, b):
+        return 0
+
+    def choice(self, lst):
+        return lst[0]
+
+def testBattler():
+    session = connect()
+
+    species = session.query(tables.Form).filter_by(id=1).one()
+    bulba = monster.Monster(species, 30, loader.natures, rand=FakeRand())
+    bulba = battler.Battler(bulba, None)
+
+    assert_equal(bulba.ability.id, 65)
+    bulba.ability = loader.loadAbility(1)
+    assert_equal(bulba.ability.id, 1)
+    assert_equal(bulba.monster.ability.id, 65)
+
+    assert_equal(bulba.gender.identifier, 'female')
+    assert_equal(
+            [m.kind.identifier for m in bulba.moves],
+            'double-edge growth sweet-scent razor-leaf'.split(),
+        )
+    bulba.setMove(0, loader.loadMove(1))
+    assert_equal(
+            [m.kind.identifier for m in bulba.moves],
+            'pound growth sweet-scent razor-leaf'.split(),
+        )
+    assert_equal(
+            [m.kind.identifier for m in bulba.monster.moves],
+            'double-edge growth sweet-scent razor-leaf'.split(),
+        )
+
+    assert_equal(bulba.tameness, 70)
+    assert bulba.name.endswith('saur')
+
+    assert not bulba.fainted
+    bulba.hp = 0
+    assert bulba.fainted
+    assert bulba.monster.fainted
+
+    assert bulba.status == monster.Status.ok
+    bulba.status = monster.Status.par
+    assert bulba.status == monster.Status.par
+    assert bulba.monster.status == monster.Status.par
+
+    assert_equal(bulba.item, None)
+    bulba.item = loader.loadItem(30)
+    assert_equal(bulba.item.id, 30)
+    assert_equal(bulba.monster.item.id, 30)
+
+    assert_equal(bulba.level, 30)
+    bulba.level = 10
+    assert_equal(bulba.level, 10)
+    assert_equal(bulba.monster.level, 30)
